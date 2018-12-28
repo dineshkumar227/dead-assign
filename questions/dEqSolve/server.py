cdfd810aa3720d66795d0f9b8c1420e46ae46856
dineshkumar227 <dkr2@illinois.edu>
2018-12-28 12:38:22 +0530

    Finished DiffEq solver

---
+++ questions/dEqSolve/server.py
@@ -0,0 +1,10 @@
+import sympy as sy
+from random import randint
+
+x = sy.symbols('x')
+f = sy.Function('f')
+diffeq = sy.Eq(f(x).diff(x) - randint(1, 9)*f(x), randint(1,9)*x)
+print("Solving: ")
+print(sy.simplify(diffeq))
+print(sy.simplify(sy.dsolve(diffeq, f(x))))
+
