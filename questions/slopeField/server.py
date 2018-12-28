113ba274268b38e437f69e602bd2e76f037630bf
dineshkumar227 <dkr2@illinois.edu>
2018-12-28 11:23:06 +0530

    Finished slopefield generation

--- questions/slopeField/server.py
+++ questions/slopeField/server.py
@@ -1,32 +1,65 @@
 import numpy as np
 from matplotlib import pyplot as plt
+from io import BytesIO
 
 # Differential equation
-# diff = y'= y/x (or say x+y)
 def diff(x,y,coeffs):
     return coeffs[0]*x**2 - coeffs[1]*y**2  # ax^2 - by^2
 
+def randomize():
+    #generate 2 random coefficients from -9 to 9 
+    coeffs = np.random.random_integers(-9,9,2)
 
+    #dont want 0's do we?
+    if coeffs[0]==0:
+        coeffs[0]=1
+    if coeffs[1]==0:
+        coeffs[1]=1
+    return coeffs
+
+
+#plots slope fields and returns a bitstream
+def plot(coeffs):
+    img = BytesIO()
+
+    for j in x:
+        for k in y:
+            domain = np.linspace(j-0.07,j+0.07,2)
+            slope = diff(j,k,coeffs)
+
+            def fun(x1,y1):
+                z = slope*(domain-x1)+y1
+                return z
+
+            plt.plot(domain,fun(j,k),solid_capstyle='projecting',solid_joinstyle='bevel')
+
+    plt.grid(True)
+    plt.show()
+    plt.savefig(img, bbox_inches='tight')
+    img.seek(0)
+
+    return img
+
+#setting extent of graph and number of samples
 x = np.linspace(-1,1,15)
 y = np.linspace(-1,1,15)
 
-#generate 2 random coefficients from -9 to 9 
-coeffs = np.random.random_integers(-9,9,2)
-if coeffs[0]==0:
-    coeffs[0]=1
+#generating answer plot
+answer_coeffs = randomize()
+answer_plot = plot(answer_coeffs)
 
-# use x,y
-for j in x:
-    for k in y:
-        slope = diff(j,k,coeffs)
-        domain = np.linspace(j-0.07,j+0.07,2)
-        def fun(x1,y1):
-            z = slope*(domain-x1)+y1
-            return z
-        plt.plot(domain,fun(j,k),solid_capstyle='projecting',solid_joinstyle='bevel')
+option_plots = []
 
-plt.grid(True)
-plt.show()
-#plt.savefig('foo.png', bbox_inches='tight')
-    
+#generating other plots for options
+for i in range(3):
+    coeffs = randomize()
+    print(coeffs)
+
+    #avoiding identical options
+    while(coeffs[0] == answer_coeffs[0]):
+        coeffs = randomize()
+    option_plots.append(plot(coeffs))
+
+#TODO embed these png bitstreams as images in question html
+
 print("End of the program")
