2d9e8fdb60219130554c1e73dbafdcf89753bd7f
dineshkumar227 <dkr2@illinois.edu>
2018-12-29 15:08:21 +0530

    Some housekeeping

--- questions/dEqClassify/server.py
+++ questions/dEqClassify/server.py
@@ -1,27 +1,30 @@
-import sympy as sy
+import sympy as sym
 
-x = sy.Symbol('x')
-f = sy.Function('f')
+x = sym.Symbol('x')
+f = sym.Function('f')
 
 nth = {
     1: "first",
     2: "second",
     3: "third",
     4: "fourth"
-    # etc
+    # add more if needed
 }
 
+#looks through tuple returned by classify_ode for instances of "linear"
 def linear_check(dffEq):
-    diffEq_class = sy.classify_ode(diffEq, f(x))
+    diffEq_class = sym.classify_ode(diffEq, f(x))
     if any("linear" in str_ for str_ in diffEq_class):
         return "linear"
     return "non-linear"
 
-dEq_list = [sy.Eq(f(x).diff(x), f(x)**2 - x**2), 
-            sy.Eq(f(x).diff(x,x), f(x) + sy.exp(x)),
-            sy.Eq(f(x).diff(x,x,x,x), f(x).diff(x,x) + f(x)),
-            sy.Eq(f(x) * f(x).diff(x,x) - 2*f(x).diff(x), sy.tan(x))]
+
+#TODO: Automate this instead of having a hardcoded list or use a pre-made list
+dEq_list = [sym.Eq(f(x).diff(x), f(x)**2 - x**2), 
+            sym.Eq(f(x).diff(x,x), f(x) + sym.exp(x)),
+            sym.Eq(f(x).diff(x,x,x,x), f(x).diff(x,x) + f(x)),
+            sym.Eq(f(x) * f(x).diff(x,x) - 2*f(x).diff(x), sym.tan(x))]
 
 for diffEq in dEq_list:
     print(linear_check(diffEq))
-    print(nth[sy.ode_order(diffEq, f(x))] + " order")
+    print(nth[sym.ode_order(diffEq, f(x))] + " order")
--- questions/matrixExpoential/server.py
+++ questions/matrixExpoential/server.py
@@ -1,11 +1,17 @@
 import scipy.linalg as sla
 import numpy as np
 
+#range of allowed values, dont want this to be too big to avoid cubersome problems
 nMax = 2
+#dimensions of matrix
 matrixRows = matrixCols = 3
 
+#generating a random matrix
 matrix = np.random.random_integers(-nMax,nMax,(matrixRows,matrixCols))
-print (sla.expm(matrix))
+
+answer = sla.expm(matrix)
+
+print(answer)
 
 
 
