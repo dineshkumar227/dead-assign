d1d03efc76056e96c551bc831c2a09c355c9cb7a
dineshkumar227 <dkr2@illinois.edu>
2018-12-30 07:24:09 +0530

    Added randomisation

--- questions/dEqMaxima/server.py
+++ questions/dEqMaxima/server.py
@@ -1,10 +1,12 @@
 import sympy as sym
+from random import randint
 
 x = sym.Symbol('x')
 f = sym.Function('f')
+initial_velocity = randint(1,9)
 
 diffeq = sym.Eq(f(x).diff(x,2), 3 * f(x).diff(x) - 2*f(x))
-conditions = {f(0):2, f(x).diff(x).subs(x, 0): 2}
+conditions = {f(0):2, f(x).diff(x).subs(x, 0): initial_velocity}
 
 print(sym.simplify(sym.dsolve(diffeq, f(x), ics = conditions)))
 
