5ca0feb29b948ca9a794fa2e2608e81466fd64c9
dineshkumar227 <dkr2@illinois.edu>
2019-01-09 21:34:21 +0530

    Finished html and json for initial value problem

--- questions/dEqMaxima/server.py
+++ questions/dEqMaxima/server.py
@@ -2,28 +2,32 @@ import sympy as sym
 import scipy.optimize
 from random import randint
 
-x = sym.Symbol('x')
-f = sym.Function('f')
-initial_displacement = randint(0,9)
-initial_velocity = randint(0,9)
-correct_answer = "DNE"
+def generate(data):
+    x = sym.Symbol('x')
+    f = sym.Function('f')
+    initial_displacement = randint(0,9)
+    initial_velocity = randint(0,9)
+    answer = "DNE"
 
 # f"(x) - 3f'(x) + 2f(x) = 0
-diffeq = f(x).diff(x,2) - 3 * f(x).diff(x) + 2*f(x)
+    diffeq = f(x).diff(x,2) - 3 * f(x).diff(x) + 2*f(x)
 #randomised starting displacment and initial velocity
-conditions = {f(0): initial_displacement, f(x).diff(x).subs(x, 0): initial_velocity}
+    conditions = {f(0): initial_displacement, f(x).diff(x).subs(x, 0): initial_velocity}
 #finding f(x) with given initial conditions, using rhs as dsolve returns and equation
-solution = sym.simplify(sym.dsolve(diffeq, f(x), ics = conditions)).rhs
+    solution = sym.simplify(sym.dsolve(diffeq, f(x), ics = conditions)).rhs
 #converting function to scipy compatible format
-fun = sym.lambdify(x, solution, "numpy")
+    fun = sym.lambdify(x, solution, "numpy")
 
 #finding maxima by finding the minimum of the negative of the function
-max_x = scipy.optimize.fmin(lambda x: -fun(x), 0, full_output = True)
-warnflag = max_x[4]
-maxima = max_x[0]
+    max_x = scipy.optimize.fmin(lambda x: -fun(x), 0, full_output = True)
+    warnflag = max_x[4]
+    maxima = max_x[0]
 
-if warnflag == 0 and maxima > 0:
-    correct_answer = maxima
-
-print(solution)
-print(correct_answer)
+    if warnflag == 0 and maxima > 0:
+        answer = str(maxima)
+    data["correct_answers"]["maxima"] = answer
+    data["params"]["df"] = sym.latex(diffeq)
+    data["params"]["init_disp"] = initial_displacement
+    data["params"]["init_vel"] = initial_velocity
+    
+    return data
