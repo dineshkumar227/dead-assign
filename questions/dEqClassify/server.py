e42d1e985ae95ae31f6ded6f0fc9ad68ba8c8dc3
dineshkumar227 <dkr2@illinois.edu>
2019-01-09 20:17:11 +0530

    Fixed equations not being JSON serializable

--- questions/dEqClassify/server.py
+++ questions/dEqClassify/server.py
@@ -12,15 +12,15 @@ def linear_check(diffEq):
 
 def generate(data):
 #TODO: Automate this instead of having a hardcoded list or use a pre-made list
-    dEq_list = [sym.Eq(f(x).diff(x), f(x)**2 - x**2), 
-                sym.Eq(f(x).diff(x,x), f(x) + sym.exp(x)),
-                sym.Eq(f(x).diff(x,4), f(x).diff(x,x) + f(x)),
-                sym.Eq(f(x) * f(x).diff(x,x) - 2*f(x).diff(x), sym.tan(x)),
-                sym.Eq(x**2 * f(x).diff(x,4) + 2*f(x)**3 * f(x).diff(x,x) - sym.exp(x)*f(x), 17)]
+    dEq_list = [sym.Eq(f(x).diff(x) - f(x)**2 + x**2, 0), 
+                sym.Eq(f(x).diff(x,x) - f(x) - sym.exp(x), 0),
+                sym.Eq(f(x).diff(x,4) - f(x).diff(x,x) - f(x), 0),
+                sym.Eq(f(x) * f(x).diff(x,x) - 2*f(x).diff(x) - sym.tan(x), 0),
+                sym.Eq(x**2 * f(x).diff(x,4) + 2*f(x)**3 * f(x).diff(x,x) - sym.exp(x)*f(x) - 17, 0)]
 
     for i in range(len(dEq_list)):
         equation_name = chr(97+i)
-        data["params"][equation_name] = dEq_list[i]
+        data["params"][equation_name] = dEq_list[i].lhs
         data["correct_answers"][equation_name+"_l"] = linear_check(dEq_list[i])
         data["correct_answers"][equation_name+"_o"] = sym.ode_order(dEq_list[i], f(x))
 
