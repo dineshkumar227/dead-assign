7ef84aee75663bb919b514a53d778c4897af106e
dineshkumar227 <dkr2@illinois.edu>
2019-01-09 17:32:44 +0530

    Fixed typo in folder name

--- questions/dEqClassify/server.py
+++ questions/dEqClassify/server.py
@@ -3,14 +3,6 @@ import sympy as sym
 x = sym.Symbol('x')
 f = sym.Function('f')
 
-nth = {
-    1: "first",
-    2: "second",
-    3: "third",
-    4: "fourth"
-    # add more if needed
-}
-
 #looks through tuple returned by classify_ode for instances of "linear"
 def linear_check(dffEq):
     diffEq_class = sym.classify_ode(diffEq, f(x))
@@ -26,6 +18,8 @@ dEq_list = [sym.Eq(f(x).diff(x), f(x)**2 - x**2),
             sym.Eq(f(x) * f(x).diff(x,x) - 2*f(x).diff(x), sym.tan(x)),
             sym.Eq(x**2 * f(x).diff(x,4) + 2*f(x)**3 * f(x).diff(x,x) - sym.exp(x)*f(x), 17)]
 
-for diffEq in dEq_list:
-    print(linear_check(diffEq))
-    print(nth[sym.ode_order(diffEq, f(x))] + " order")
+for i in range(len(dEq_list)):
+    equation_name = chr(i)
+    data["params"][equation_name] = dEq_list[i]
+    data["correct_answers"][equation_name+"_l"] = linear_check(dEq_list[i])
+    data["correct_answers"][equation_name+"_o"] = sym.ode_order(dEq_list[i], f(x))
