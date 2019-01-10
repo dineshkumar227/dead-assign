f50e0f8abcd7333aecefee8e8afbcccbb3555cef
dineshkumar227 <dkr2@illinois.edu>
2019-01-10 09:50:21 +0530

    All questions except slope field and init value working

--- questions/dEqSolve/server.py
+++ questions/dEqSolve/server.py
@@ -1,14 +1,18 @@
-import sympy as sy
+import sympy as sym
 from random import randint
 import prairielearn as pl
 
 def generate(data):
-    x = sy.symbols('x')
-    f = sy.Function('f')
+    x = sym.symbols('x')
+    f = sym.Function('f')
+    A = sym.symbols('A')
+    C1 = sym.symbols('C1')
+    diffeq = sym.Eq(f(x).diff(x) - randint(1, 9)*f(x), randint(1,9)*x)
+    ans = sym.simplify(sym.dsolve(diffeq, f(x)).rhs)
+    ans = ans.subs(C1, A)
 
-    diffeq = sy.Eq(f(x).diff(x) - randint(1, 9)*f(x), randint(1,9)*x)
-
-    data["params"]["a"] = diffeq
-    data['correct_answer']["ans"] = pl.to_json(sy.simplify(sy.dsolve(diffeq, f(x))))
-
+    data["params"]["a"] = sym.latex(diffeq)
+    data["correct_answers"]["ans"] = pl.to_json(ans)
+    data["params"]["x"] = sym.latex(x)
+    data["params"]["A"] = sym.latex(A)
     return data
--- questions/dEqSolveParam/server.py
+++ questions/dEqSolveParam/server.py
@@ -1,12 +1,21 @@
-import sympy as sy
+import sympy as sym
 from random import randint
 import prairielearn as pl
 
 def generate(data):
-    x = sy.symbols('x')
-    f = sy.Function('f')
-    diffeq = sy.Eq(f(x).diff(x,x) + f(x), sy.sec(x))
-    data["params"]["a"] = sym.latex(sy.simplify(diffeq))
-    data['correct_answer']["ans"] = pl.to_json(sy.simplify(sy.dsolve(diffeq, f(x), hint = 'nth_linear_constant_coeff_variation_of_parameters')))
+    x = sym.symbols('x')
+    f = sym.Function('f')
+    C1, C2 = sym.symbols('C1, C2')
+    A = sym.symbols('A')
+    B = sym.symbols('B')
+    diffeq = sym.Eq(f(x).diff(x,x) + f(x), sym.sec(x))
+    ans = (sym.simplify(sym.dsolve(diffeq, f(x), hint = 'nth_linear_constant_coeff_variation_of_parameters').rhs))
+    ans = ans.subs([(C1, A), (C2,B)])
+
+    data["params"]["x"] = sym.latex(x)
+    data["params"]["a"] = sym.latex(sym.simplify(diffeq))
+    data["correct_answers"]["ans"] = pl.to_json(ans)
+    data["params"]["A"] = sym.latex(A)
+    data["params"]["B"] = sym.latex(B)
 
     return data
