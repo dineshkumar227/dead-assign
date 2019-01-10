c43f715bf2f1c1ccaac60e43193ff9e68e372cff
dineshkumar227 <dkr2@illinois.edu>
2019-01-10 20:14:42 +0530

    hardcoding maxima question till pl is updated

--- questions/dEqMaxima/server.py
+++ questions/dEqMaxima/server.py
@@ -1,31 +1,15 @@
 import sympy as sym
-import numpy
-import scipy.optimize
-from random import randint
 
 def generate(data):
     x = sym.Symbol('x')
     f = sym.Function('f')
-    initial_displacement = randint(0,9)
-    initial_velocity = randint(0,9)
-    answer = "DNE"
+    initial_displacement = 0
+    initial_velocity = 2
 
-# f"(x) - 3f'(x) + 2f(x) = 0
-    diffeq = f(x).diff(x,2) - 3 * f(x).diff(x) + 2*f(x)
-#randomised starting displacment and initial velocity
-    conditions = {f(0): initial_displacement, f(x).diff(x).subs(x, 0): initial_velocity}
-#finding f(x) with given initial conditions, using rhs as dsolve returns and equation
-    solution = sym.simplify(sym.dsolve(diffeq, f(x), ics = conditions)).rhs
-#converting function to scipy compatible format
-    fun = sym.lambdify(x, solution, "numpy")
+# f"(x) + 3f'(x) + 2f(x) = 0
+    diffeq = f(x).diff(x,2) + 3 * f(x).diff(x) + 2*f(x)
 
-#finding maxima by finding the minimum of the negative of the function
-    optimize_output = scipy.optimize.minimize(lambda x: fun(x), 0)
-    maxima = optimize_output.x[0]
-
-    if optimize_output.success == True and maxima > 0:
-        answer = str(round(maxima[0], 2))
-    data["correct_answers"]["ans"] = answer
+    data["correct_answers"]["ans"] = 0.69315 
     data["params"]["df"] = sym.latex(diffeq)
     data["params"]["init_disp"] = initial_displacement
     data["params"]["init_vel"] = initial_velocity
---
+++ questions/dEqMaxima/shelved_server.py
@@ -0,0 +1,42 @@
+import sympy as sym
+from sympy.solvers.ode import solve_ics
+import numpy
+import scipy.optimize
+from random import randint
+
+def generate(data):
+    x = sym.Symbol('x')
+    f = sym.Function('f')
+    initial_displacement = randint(0,9)
+    initial_velocity = randint(0,9)
+    answer = "DNE"
+
+# f"(x) - 3f'(x) + 2f(x) = 0
+    diffeq = f(x).diff(x,2) - 3 * f(x).diff(x) + 2*f(x)
+#randomised starting displacment and initial velocity
+    conditions = {f(0): initial_displacement, f(x).diff(x).subs(x, 0): initial_velocity}
+#finding f(x) with given initial conditions, using rhs as dsolve returns and equation
+    solution = sym.simplify(sym.dsolve(diffeq, f(x), ics = conditions)).rhs
+    print(solution)
+#converting function to scipy compatible format
+    fun = sym.lambdify(x, solution, "numpy")
+    my_fun = lambda a: numpy.exp(a) - 6
+    print("fun type:")
+    print(type(fun))
+    print("my_fun type:")
+    print(type(my_fun))
+
+    print(fun(0.1))
+
+#finding maxima by finding the minimum of the negative of the function
+    optimize_output = scipy.optimize.minimize(lambda x: fun(x), 0)
+    maxima = optimize_output.x[0]
+
+    if optimize_output.success == True and maxima > 0:
+        answer = str(round(maxima[0], 2))
+    data["correct_answers"]["ans"] = answer
+    data["params"]["df"] = sym.latex(diffeq)
+    data["params"]["init_disp"] = initial_displacement
+    data["params"]["init_vel"] = initial_velocity
+    
+    return data
